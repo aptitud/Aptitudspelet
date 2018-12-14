8e190ef72e46eccd3416a7a345af331be618ac0f
Christian Fredh <christian.fredh@dustin.se>
2018-12-14 10:57:04 +0100

    Basic player controls

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerControls : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+    private Rigidbody2D rigidBody;
+    private float movement = 0f;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement = Input.GetAxis("Vertical") * movementSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        var velocity = rigidBody.velocity;
+        velocity.y = movement;
+        rigidBody.velocity = velocity;
+    }
+}
