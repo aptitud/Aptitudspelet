4e1e3794625d0f2dde8919628cfd29fb10fd377c
Christian Fredh <christian.fredh@dustin.se>
2018-12-14 13:13:16 +0100

    Obsticle spawner

---
+++ Assets/ObsticleSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ObsticleSpawner : MonoBehaviour
+{
+    private Transform obsticleContainer;
+    private float spawnTime = 0.3f;
+    private float timeToLive = 5f;
+    private Transform[] spawnPoints;
+
+    public GameObject obsticlePrefab;
+    
+    void Start()
+    {
+        obsticleContainer = this.gameObject.transform.Find("Obsticles");
+
+        var spawnPointContainer = this.gameObject.transform.Find("SpawnPoints");
+        spawnPoints = spawnPointContainer.Cast<Transform>().ToArray();
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+
+    void Spawn()
+    {
+        // If the player has no health left...
+        //if(playerHealth.currentHealth <= 0f)
+        //{
+        //    // ... exit the function.
+        //    return;
+        //}
+
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        var obsticle = Instantiate(obsticlePrefab, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        obsticle.transform.parent = obsticleContainer;
+
+        Destroy(obsticle, timeToLive);
+    }
+}
