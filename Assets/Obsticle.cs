235c1f85e48b278e644c6c1b0f1f6f5f1cac6253
Christian Fredh <christian.fredh@dustin.se>
2018-12-14 12:28:23 +0100

    Obsticle movement

---
+++ Assets/Obsticle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Obsticle : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+    private Rigidbody2D rigidBody;
+    private float movement = 10f;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        var velocity = rigidBody.velocity;
+        velocity.x = -movement;
+        rigidBody.velocity = velocity;
+    }
+}
