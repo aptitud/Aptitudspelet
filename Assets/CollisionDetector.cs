3392a3222aa06fda3578bd09645966a740986414
Christian Fredh <christian.fredh@dustin.se>
2018-12-14 13:53:18 +0100

    Restart game if obsticle is hit

---
+++ Assets/CollisionDetector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionDetector : MonoBehaviour
+{
+    private Renderer renderer;
+    private IEnumerable<Renderer> childRenderers;
+
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        childRenderers = GetComponentsInChildren<Renderer>(true);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Obsticle")
+        {
+            renderer.enabled = false;
+            foreach (var childRenderer in childRenderers)
+            {
+                childRenderer.enabled = false;
+            }
+
+            StartCoroutine(Restart());
+            Destroy(gameObject, 2);
+        }
+    }
+
+    IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(1);
+        GameManager.StartNewGame();
+    }
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class GameManager
+{
+    public static void StartNewGame()
+    {
+        Debug.Log("Starting new game");
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+}
