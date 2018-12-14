ee6ebfb08101d18cd76530dbff8c4c6fa3950248
Christian Fredh <christian.fredh@dustin.se>
2018-12-14 15:19:55 +0100

    Change colors of logos

---
+++ Assets/LogoRandomizer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoRandomizer : MonoBehaviour
+{
+    public Sprite[] sprites;
+    
+	void Start ()
+	{
+	    var spriteIndex = Random.Range(0, sprites.Length);
+
+        var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+	    spriteRenderer.sprite = sprites[spriteIndex];
+	}
+}
